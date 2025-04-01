5e7131686c2076b83bf3eb1ff86d9059a7f4654d
glebocrew <62657311+glebocrew@users.noreply.github.com>
2025-04-01 14:44:35 +0300

    login-completed

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-import pandas
+from back import *
+import pandas as pd
 
 app = Flask(__name__)
+app.secret_key = "SPDKfNSkdkf"
 
-
+@app.route("/")
+def index():
+    return redirect("/login")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    users = {}  # GOOGLE TABLE HERE #
+    users = pd.read_html('https://docs.google.com/spreadsheets/d/1qJ3veQPbXTwN21M7Ovl9ITWzkk5zaSJAVoB1_5FxfLw/edit?usp=sharing')
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
-        if username in users and check_password_hash(users[username], password):
+        if сравни_эту_хуету(username, password)["значение"] == "норм":
             session['username'] = username
-            flash('вечер в хату', 'success')
             return redirect(url_for('dashboard'))
         else:
-            flash('сори че то не то', 'danger')
+            return render_template('login.html', message = "хуйня")
 
     return render_template('login.html')
 
 
-@app.route('/enter')
+@app.route('/enter', methods=["GET"])
 def dashboard():
-    users = {}  # GOOGLE TABLE HERE #
-    username = request.form['username']
-    if username not in users:
-        return (redirect('https://docs.google.com/spreadsheets/d/1qJ3veQPbXTwN21M7Ovl9ITWzkk5zaSJAVoB1_5FxfLw/edit?usp=sharing'))
-        # редиректим на гугл таблицу для реги
-    return render_template('dashboard.html', username=username)
+    return render_template('dashboard.html')
+
+if __name__ == "__main__":
+    app.run()
